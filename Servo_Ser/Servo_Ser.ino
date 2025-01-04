ed0debd3cc07dac5b4dc0bfa196760d096a58aa2
A-N-M-Noor <nafis.noor.202012@gmail.com>
2025-01-04 12:04:06 +0600

    new file:   Servo_Ser/Servo_Ser.ino

---
+++ Servo_Ser/Servo_Ser.ino
@@ -0,0 +1,42 @@
+//#include <Servo.h>
+#include <ESP32Servo.h>
+
+Servo s;
+
+int target = 0, current = 90;
+int angSpd = 1;
+int angDel = 20;
+int stabilizeDel = 500;
+
+bool reached = true;
+
+void setup() {
+  Serial.begin(9600);
+//  s.attach(9);
+  s.attach(16);
+}
+
+void loop() {
+  while(Serial.available()>0){
+    target = Serial.readStringUntil('\n').toInt();
+    reached = false;
+  }
+
+  while(current != target){
+    if(abs(current - target) > angSpd){
+      current += (target > current)? angSpd : -angSpd;
+    }
+    else{
+      current = target;
+    }
+    s.write(int(current));
+    delay(angDel);
+  }
+
+  if(current == target && reached == false){
+    reached = true;
+    delay(stabilizeDel);
+    Serial.println("done");
+  }
+  
+}
